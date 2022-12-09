09dbd7471a0fc109c6cbc6d2bf26c2faf3dff26e
Semyon Zakopay <crimsonglovv.working@gmai.com>
2022-12-09 23:56:04 +0200

    handler enhancement

--- app/presenters/review_presenter.rb
+++ app/presenters/review_presenter.rb
@@ -5,18 +5,23 @@ class ReviewPresenter < Rectify::Presenter
   MIN_STARS = 1
 
   def all_reviews
-    current_book.reviews.includes([:user]).order('created_at DESC')
+    current_reviews.order('created_at DESC')
   end
 
   def quantity_reviews
-    current_book.reviews.count
+    current_reviews.count
   end
 
   def verified?(review)
-    t('book_page.review.verified_reviewer') if review.is_verified
+    t('book_page.review.verified_reviewer') if review.is_verified && review.user.orders.present?
   end
 
   def check_star(star, review)
     review.rating && star < review.rating ? 'rate-star' : 'star-not-checked'
+  end
+
+  def current_reviews
+    current_reviews = current_book.reviews.approved
+    current_reviews.empty? ? current_reviews : current_reviews.includes([:user])
   end
 end
